09881b20dd4c4d541a671b538b68f4efebd4dbcd
iapetroaei <apetroaei1@gmail.com>
2024-05-16 20:16:28 -0400

    Added Assessment 1

---
+++ Assessment1/Assessment1/Program.cs
@@ -0,0 +1,50 @@
+﻿static bool IsPassing(double grade)
+{
+    if (grade <= 65)
+    { 
+        return false; 
+    }
+    else return true;
+}
+
+Console.WriteLine(IsPassing(65));
+Console.WriteLine(IsPassing(66));
+
+
+static double AverageGrades(double grade1, double grade2, double grade3)
+{
+    return ( grade1 + grade2 + grade3) / 3 ;
+}
+
+Console.WriteLine(AverageGrades(66, 65, 75));
+
+
+
+static string OddOrEvenAndPassing(double grade4)
+{
+    int convertGrade = Convert.ToInt32(grade4);
+
+    if (IsPassing(convertGrade) == true && convertGrade % 2 == 1)
+    {
+        return "\nPassing and odd";
+    }
+    else if (IsPassing(convertGrade) == true && convertGrade % 2 == 0)
+    {
+        return "\nPassing and even";
+    }
+    else if (IsPassing(convertGrade) == false  && convertGrade % 2 == 0)
+    {
+        return "\nFailing and even";
+    }
+    else
+    {
+        return "\nFailing and odd";
+    }
+}
+
+
+
+Console.WriteLine(OddOrEvenAndPassing(63));
+Console.WriteLine(OddOrEvenAndPassing(64));
+Console.WriteLine(OddOrEvenAndPassing(68));
+Console.WriteLine(OddOrEvenAndPassing(67));
---
+++ BasicForLoops/BasicForLoops/Program.cs
@@ -0,0 +1,22 @@
+﻿bool continueLoop = true; // Initialize continueLoop to true to enter the loop at least once
+
+        while (continueLoop)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // Calculate the sum of all numbers from 1 to the entered number using just a for loop
+            int sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"The sum of all numbers from 1 to {number} is: {sum}");
+
+            Console.Write("Would you like to continue (y/n)? ");
+            string input = Console.ReadLine().ToLower();
+            continueLoop = input == "y"; // Update continueLoop based on user input
+        }
+
+        Console.WriteLine("Goodbye!");
